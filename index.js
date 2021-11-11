22ed183792084ee55caa69efb1ee72651510807c
sheep0317 <tranvietmar15@gmail.com>
2021-11-11 09:14:55 +0700

    S3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var AWS = require('aws-sdk');
-
+const fs = require('fs');
 require('dotenv').config();
 AWS.config.update({
     region: process.env.AWS_REGION,
@@ -7,8 +7,21 @@ AWS.config.update({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     sessionToken: process.env.AWS_SESSION_TOKEN
 });
-var namefile = 'test.jpg';
-
+var NAMEFILE = '053.jpg';
+var BUCKET = 'kalsitbucket';
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN
+});
+const params = {
+    Bucket: BUCKET,
+    Key: NAMEFILE,
+    Body: fs.createReadStream('./' + NAMEFILE),
+}
+s3.upload(params, function (err, data) {
+    console.log(err, data);
+});
 /*
 const uploadFile = (fileName) => {
     // Read content from the file
@@ -30,6 +43,9 @@ const uploadFile = (fileName) => {
     });
 };
 */
+
+//done
+/*
 var params = {
     Image: {
         S3Object: {
@@ -48,3 +64,4 @@ rekognition.detectLabels(params, function (err, data) {
 })
 
 console.log("END");
+*/
